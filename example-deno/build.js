e3a335bae506dd62530395ef14607e526348913c
dalcib <dalcib@gmail.com>
2021-04-14 03:29:58 +0530

    v0.2.5

--- example-deno/build.js
+++ example-deno/build.js
@@ -1,5 +1,5 @@
 import * as esbuild from 'https://deno.land/x/esbuild/mod.js'
-import { cache } from 'https://deno.land/x/esbuild_plugin_cache/mod.ts'
+import { cache } from './../deno/mod.ts'
 
 const importmap = {
   imports: {
@@ -8,13 +8,12 @@ const importmap = {
   },
 }
 
-esbuild
-  .build({
-    entryPoints: ['./index.jsx'],
-    bundle: true,
-    format: 'esm',
-    plugins: [cache({ importmap, directory: './cache' })],
-    outfile: 'deno-bundle.js',
-    loader: { '.js': 'jsx' },
-  })
-  .catch(() => process.exit(1))
+esbuild.build({
+  entryPoints: ['./index.tsx'],
+  bundle: true,
+  format: 'esm',
+  plugins: [cache({ importmap, directory: './cache' })],
+  outfile: 'bundle.js',
+})
+
+esbuild.stop()
